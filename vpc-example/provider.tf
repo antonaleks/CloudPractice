ee2110b54e9ddeb0590b38777a8c23dcd9ea527e
malikov.at <malikoff.at@gmail.com>
2024-05-17 08:06:56 +0300

    Init

--- vpc-example/provider.tf
+++
@@ -1,9 +0,0 @@
-# Инициализация провайдера OpenStack
-provider "openstack" {
-  auth_url    = "https://api.selvpc.ru/identity/v3"
-  domain_name = var.domain_name
-  tenant_id   = var.tenant_id
-  user_name   = var.user_name
-  password    = var.password
-  region      = var.region
-}
