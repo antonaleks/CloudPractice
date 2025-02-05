f580afc84faa38e657f205ddb128f8e6f7c357ae
VladislavRkm <sl3epy.1rkm1@yandex.ru>
2025-02-05 04:54:45 +0300

    some changes

--- preemptible_server/modules/keypair/main.tf
+++ preemptible_server/modules/keypair/main.tf
@@ -1,6 +1,6 @@
 # Создание SSH-ключа
 resource "openstack_compute_keypair_v2" "key_tf" {
-  name       = "key_tf"
+  name       = "new_key_tf"
   region     = var.region
   public_key = var.keypair_public_key
 }
